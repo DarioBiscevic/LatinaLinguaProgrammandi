5b97846b351b1ea108050f856bb33ecbff21f78d
Dario Biscevic <dario.biscevic03@gmail.com>
2023-01-06 18:52:05 +0100

    Added factorial example

--- translator.sh
+++ translator.sh
@@ -23,13 +23,15 @@ filename=$2
 echo "#!/usr/bin/perl" > $filename
 echo "use strict;" >> $filename
 echo "use warnings;" >> $filename
+echo "use 5.36.0;" >> $filename
 
 # Variables
-printf "%s" "$(cat $1 | sed "s/^\s*datum\s*\([a-zA-Z0-9]*\)\s*est\s*\"\(.*\)\"/my \1=\"\2\"/g")" >> $filename
-printf "%s" "$(cat $filename | sed "s/^\s*datum\s*\([a-zA-Z0-9]*\)\s*est\s*\(.*\)/my \$\1 = \2/g")" > $filename
+printf "%s" "$(cat $1 | sed "s/^\(\s*\)datum\s*\([a-zA-Z0-9]*\)\s*est\s*\"\(.*\)\"/\1my \2=\"\3\"/g")" >> $filename
+printf "%s" "$(cat $filename | sed "s/^\(\s*\)datum\s*\([a-zA-Z0-9]*\)\s*est\s*\(.*\)/\1my \$\2 = \3/g")" > $filename
 
 # if statements
-printf "%s" "$(cat $filename | sed "s/^\s*si\s*\([\$a-zA-Z0-9]*\)\(.*\)\s*age/if(\1\2\){/g")" > $filename
+printf "%s" "$(cat $filename | sed "s/^\(\s*\)si\s*\([\$a-zA-Z0-9]*\)\(.*\)\s*age/\1if(\2\3\){/g")" > $filename
+printf "%s" "$(cat $filename | sed "s/^\(\s*\)aliter si\s*\([\$a-zA-Z0-9]*\)\(.*\)\s*age\(.*\)/\1}elsif(\2\3){/g")" > $filename
 printf "%s" "$(cat $filename | sed "s/\(.*(.*\)est maior aut aequum quam\(.*).*\)/\1\>=\2/g")" > $filename
 printf "%s" "$(cat $filename | sed "s/\(.*(.*\)est minor aut aequum quam\(.*).*\)/\1\<=\2/g")" > $filename
 printf "%s" "$(cat $filename | sed "s/\(.*(.*\)est maior quam\(.*).*\)/\1\>\2/g")" > $filename
@@ -44,10 +46,9 @@ printf "%s" "$(cat $filename | sed "s/^\(\s*\)nuntia/\1print/g")" > $filename
 printf "%s" "$(cat $filename | sed "s/^\(\s*\)percipe\s*\([^;]*\)/\1my \$\2 = <STDIN>; chomp(\$\2)/g")" > $filename
 
 # block endings
-printf "%s" "$(cat $filename | sed "s/^\s*conclude si/}/g")" > $filename
-printf "%s" "$(cat $filename | sed "s/^\s*aliter si\(.*\)/}else if \1/g")" > $filename
-printf "%s" "$(cat $filename | sed "s/^\s*aliter/}else{/g")" > $filename
-printf "%s" "$(cat $filename | sed "s/^\s*conclude iteratio/}\n/g")" > $filename
+printf "%s" "$(cat $filename | sed "s/^\(\s*\)conclude si/\1}/g")" > $filename
+printf "%s" "$(cat $filename | sed "s/^\(\s*\)aliter age/\1}else{/g")" > $filename
+printf "%s" "$(cat $filename | sed "s/^\(\s*\)conclude iteratio/\1}\n/g")" > $filename
 
 # for
 printf "%s" "$(cat $filename | sed "s/^\s*itera\s*\([\$a-zA-Z0-9]*\)\s*ab\s*\([\$a-zA-Z0-9]*\)\s*ad\s*\([\$a-zA-Z0-9]*\)/for (my \1 = \2; \1 <= \3; \1++){/g")" > $filename
@@ -57,5 +58,8 @@ printf "%s" "$(cat $filename | sed "s/\([\$a-zA-Z0-9]*\)\s*summa\s*\([\$a-zA-Z0-
 
 # functions
 printf "%s" "$(cat $filename | sed "s/\s*praecepta\s*\([a-zA-Z0-9]*\)/sub \1/g")" > $filename
+printf "%s" "$(cat $filename | sed "s/\(\s*\)redde\(.*\)/\1return\2/g")" > $filename
+
+printf "%s" "$(cat $filename | sed "s/Variabilis\([0-9]\)*/\$_\[\1\]/g")" > $filename
 
 chmod +x $filename
